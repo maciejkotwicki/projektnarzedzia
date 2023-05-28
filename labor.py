19c30487e631bb8d852568df1fdf036cf5fd1db4
Maciej <ratusek007@gmail.com>
2023-05-28 19:50:53 +0200

    Task2

--- labor.py
+++ labor.py
@@ -3,3 +3,10 @@ import json
 import yaml
 arguments = sys.argv[1:]
 print("Passed arguments:", arguments)
+json_file = "file.json"
+try:
+    with open(json_file, "r") as file:
+        data = json.load(file)
+    print("Loaded JSON data:", data)
+except Exception as e:
+    print("Failed to load JSON data from file:", e)
